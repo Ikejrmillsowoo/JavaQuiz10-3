9b828e43dd8840ee30f4646f97c5669a9abbbb7d
Isaac Mills-Owoo <isaac@Isaacs-MacBook-Pro.local>
2025-05-16 00:50:46 -0400

    palindrome evaluater is complete

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,31 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        String[] arr = string.split("");
+        Set<String> newArr = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (isPalindrome(string.substring(i,j+1))) {
+                    newArr.add(string.substring(i,j+1));
+                }
+            }
+        }
+        return newArr.toArray(new String[0]);
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        return string.equals(reverseString(string));
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder sb = new StringBuilder(string);
+        return sb.reverse().toString();
     }
 }
