4fceeea4df69b7ad7785912a37640c8746049433
Isaac Mills-Owoo <isaac@Isaacs-MacBook-Pro.local>
2025-05-16 00:24:52 -0400

    passed string assembler and workjing on string evaluator

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,15 +1,43 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.*;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
+
+
     public static String[] getAllSubstrings(String string) {
-        return null;
+        String[] arr = string.split("");
+        Set<String> newArr = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                newArr.add(string.substring(i,j+1));
+            }
+        }
+       return newArr.toArray(new String[0]);
+
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
-        return null;
+        String[] str1 = getAllSubstrings(string1);
+        String[] str2 = getAllSubstrings(string2);
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < str1.length; i++) {
+            for (int j = 0; j < str2.length; j++) {
+                if equals(str2[j])){
+                    set.add(str1[i]);
+                }
+            };
+
+        }
+
+
+
+
+
     }
 
     public static String getLargestCommonSubstring(String string1, String string2) {
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -4,14 +4,24 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
+    private char delimeter;
+    StringBuilder sb;
     public StringAssembler(Character delimeter) {
+        this.delimeter = delimeter;
+        this.sb = new StringBuilder();
     }
 
     public StringAssembler append(String str) {
-        return null;
+        if (sb.length() == 0){
+            sb.append(str);
+        } else {
+            sb.append(delimeter).append(str);
+        }
+
+        return this;
     }
 
     public String assemble() {
-        return null;
+      return sb.toString().trim();
     }
 }
